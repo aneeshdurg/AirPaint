98a5b79b13c9a23c93469284e6b9cb6531fe8f88
aneeshdurg <durg2@illinois.edu>
2015-11-24 11:27:40 -0600

    Added some featues, fixed some bugs

--- README.md
+++ README.md
@@ -1,17 +1,39 @@
 # AirPaint
-Requires openCV, pygame, numpy.
+Requires openCV, pygame, numpy and a working webcam (quality is not important).
 
-A paint application that uses openCV and blob detection as the paint brush.
+A paint application that uses openCV and blob detection to replace the paint brush.
+The frames captured from the webcam are first filtered for the color yellow and then
+scanned for dark blobs. To view the keypoints being picked up, uncomment lines 94 and
+95.
+
+Viewing the console while main.py is running will show the coordinate of the brush
+with respect to your webcam (as opposed to the pygame screen to which it is mapped).
 
 Controls:
   Brush - your "brush" (*)
+  
+  Increase brush size - up
+  Decrease brush size - down
+  Reset brush size - left or right
+
   Clear Screen - Backspace
-  Change colors - Space
-  Eraser - c
+
+  Cycle through colors - Space
+  Change to color:	
+  	Black - b
+  	Red - r
+  	Green - g
+  	Blue - b
+  	Yellow - y
+  	Purple - p
+  	Turquoise - t
+
+  Eraser - e
+  
   Exit - Escape
   
 
-*You need to make a brush. Mine is a yellow pen, with a black piece of paper attached to it.
+*You need to make the brush. Mine is a yellow pen, with a black piece of paper attached to it.
 
 ![Brush](http://i.imgur.com/K6bKWJx.jpg "Brush")
 
--- main.py
+++ main.py
@@ -5,14 +5,15 @@ import pygame
 pygame.init()
 SIZE = [1920, 1080]
 screen = pygame.display.set_mode(SIZE)
-pygame.display.set_caption("Test blobs")
+pygame.display.set_caption("Air Paint")
 screen.fill((255, 255, 255))
 done=False
 
 colors = [(0, 0, 0), (255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (255, 0, 255), (0, 255, 255), (255, 255, 255)]
+colorString = ["Black", "Red", "Green", "Blue", "Yellow", "Purple", "Turquoise", "Eraser"]
 color=0
 brushSize = 5
-eraser = [False, 0]
+eraser = [False, 0, 5]
 
 cap = cv2.VideoCapture(0)
 x, y=0, 0
@@ -22,25 +23,61 @@ while not done:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 				done = True
+	if pygame.key.get_pressed()[pygame.K_ESCAPE]:
+		exit()
+	if pygame.key.get_pressed()[pygame.K_UP]:
+		eraser[2]+=1
+		brushSize = eraser[2]
+
+	if pygame.key.get_pressed()[pygame.K_DOWN]:
+		eraser[2]-=1
+		if eraser[2]<0:
+			eraser[2] = 0
+		brushSize = eraser[2]
+		
+	if pygame.key.get_pressed()[pygame.K_LEFT] or pygame.key.get_pressed()[pygame.K_RIGHT]:
+		eraser[2]=5	
+		brushSize = eraser[2]
+
 	if pygame.key.get_pressed()[pygame.K_SPACE]:
 		color+=1
 		color%=(len(colors)-1)
+		brushSize = eraser[2]
+
+	if pygame.key.get_pressed()[pygame.K_b]:
+		color = 0
+	if pygame.key.get_pressed()[pygame.K_r]:
+		color = 1
+	if pygame.key.get_pressed()[pygame.K_g]:
+		color = 2
+	if pygame.key.get_pressed()[pygame.K_b]:
+		color = 3
+	if pygame.key.get_pressed()[pygame.K_y]:
+		color = 4
+	if pygame.key.get_pressed()[pygame.K_p]:
+		color = 5
+	if pygame.key.get_pressed()[pygame.K_t]:
+		color = 6
+
 	if pygame.key.get_pressed()[pygame.K_BACKSPACE]:
-		screen.fill((255, 255, 255))	
-	if pygame.key.get_pressed()[pygame.K_ESCAPE]:
-		exit()
-	if pygame.key.get_pressed()[pygame.K_c]:
+		screen.fill((255, 255, 255))
+		eraser[2] = 5
+		brushSize = eraser[2]
+
+	if pygame.key.get_pressed()[pygame.K_e]:
 		if eraser[0]:
 			eraser[0] = False
 			color = eraser[1]
-			brushSize = 5
+			brushSize = eraser[2]
 
 		else:
 			eraser[0] = True
 			eraser[1] = color
+			eraser[2] = brushSize
 			color = len(colors)-1
-			brushSize = 10
+			brushSize = brushSize + 5
 
+	pygame.display.set_caption("Air Paint Color: "+colorString[color]+", Brush Size: "+str(brushSize))
 	_, frame = cap.read()
 	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 	lower = np.array([20,20,20])
